948380af8e9c5ab158a50fd5f803283ce563ed3b
vovadream <46814443+vovadream@users.noreply.github.com>
2019-01-29 09:34:13 +0500

    Update installopencv.sh

--- installopencv.sh
+++ installopencv.sh
@@ -21,11 +21,11 @@ OPENCV_VERSION='2.4.13.5'
  
 # 1. KEEP UBUNTU OR DEBIAN UP TO DATE
  
-#apt-get -y update
-#apt-get -y upgrade
-#apt-get -y dist-upgrade
+apt-get -y update
+apt-get -y upgrade
+apt-get -y dist-upgrade
 apt-get -y autoremove
- 
+apt install -y dialog apt-utils 
  
 # 2. INSTALL THE DEPENDENCIES
  
